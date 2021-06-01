730e160c7b39b19bc6c0757fb6a363889080a459
Christian Still <u19060123@tuks.co.za>
2021-06-01 18:26:42 +0200

    Created the User entity

---
+++ Backend/src/User/user.entity.ts
@@ -0,0 +1,20 @@
+import { Office } from "src/Office/office.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+    
+    @Column()
+    lastName: string;
+
+    @Column()
+    userName: string;
+
+    @ManyToMany(type => Office, office => office.users)
+    ofices: Office[];
+}
