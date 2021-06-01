48280c23be03c8e8805a0a87b91100cd1c90d616
Christian Still <u19060123@tuks.co.za>
2021-06-02 00:43:19 +0200

    Added jwt functionality

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -11,13 +11,13 @@ import { Office } from './office/office.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '176.58.102.89',
-      port: 3306,
-      username: 'System',
-      password: 'audiosuitetest',
-      database: 'AUDIO_SUITE',
+      host: '176.58.102.89', // TODO change to env var
+      port: 3306, // TODO change to env var
+      username: 'System', // TODO change to env var
+      password: 'audiosuitetest', // TODO change to env var
+      database: 'AUDIO_SUITE', // TODO change to env var
       entities: [Office, User],
-      synchronize: true,}),
+      synchronize: true,}), // TODO change to false for production
     UserModule, 
     OfficeModule],
   controllers: [AppController],
