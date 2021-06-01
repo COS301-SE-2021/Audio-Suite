6b4c0b6ac294b8cb65f8158368f959245edd104a
Christian Still <u19060123@tuks.co.za>
2021-06-01 20:49:30 +0200

    Added User modules, controller and services

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OfficeModule } from './office/office.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [UserModule, OfficeModule],
   controllers: [AppController],
   providers: [AppService],
 })
