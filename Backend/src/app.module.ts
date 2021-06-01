5dac33cf509e06848a021f20ed588eba6ec3c469
Christian Still <u19060123@tuks.co.za>
2021-06-01 21:41:22 +0200

    Connected the app to the root of the database

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -5,9 +5,21 @@ import { UserModule } from './user/user.module';
 import { OfficeModule } from './office/office.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { Office } from './office/office.entity';
 
 @Module({
-  imports: [UserModule, OfficeModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '176.58.102.89',
+      port: 3306,
+      username: 'System',
+      password: 'audiosuitetest',
+      database: 'AUDIO_SUITE',
+      entities: [Office, User],
+      synchronize: true,}),
+    UserModule, 
+    OfficeModule],
   controllers: [AppController],
   providers: [AppService],
 })
