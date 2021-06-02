7bb68a13850d7c0897755b4ca9af32636a06ccb1
u19126222 <u19126222@tuks.co.za>
2021-06-02 21:51:57 +0200

    Initialised room.module.ts

---
+++ Backend/src/room/room.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomController } from './room.controller';
+import { Room } from './room.entity';
+import { RoomService } from './room.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Room])],
+  controllers: [RoomController],
+  providers: [Room, RoomService]
+})
+export class RoomModule {}
