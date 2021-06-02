7c2e984bda98a15e87d2a039237c08b9490b4f5e
0xWra1th <danielscragg@outlook.com>
2021-06-02 14:58:46 +0200

    Office Service Update

--- Backend/src/office/office.service.ts
+++ Backend/src/office/office.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from "crypto";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Office } from './office.entity';
@@ -7,10 +8,22 @@ import { Office } from './office.entity';
 export class OfficeService {
     constructor(
         @InjectRepository(Office)
-        private usersRepository: Repository<Office>,
+        private officesRepository: Repository<Office>,
         ) {}
 
     getOffice(): string {
         return 'Hello World, Office!';
+    }
+
+    async registerOffice(name: string): Promise<Office>{
+        const invite = randomUUID();
+        try{
+            const office = await this.officesRepository.create({name, invite});
+            const savedOffice = await this.officesRepository.save(office);
+            return savedOffice;
+        }
+        catch(err) {{
+            throw err;
+        }}
     }
 }
