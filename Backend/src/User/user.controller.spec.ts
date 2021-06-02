8883f98e07d5df3eb02b5a2a6ba5b5e704313afb
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-06-03 00:15:48 +0200

    Commit for registerUser Test in user.controller.spec.ts

--- Backend/src/User/user.controller.spec.ts
+++ Backend/src/User/user.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUserService = {
+    registerUser: jest.fn((firstName, lastName, userName, email, password) => {
+      return {
+        id: Date.now(),
+        firstName: firstName,
+        password: password,
+        email: email,
+        userName: userName,
+        lastName: lastName,
+      };
+    }),
+
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      providers: [UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should make use of registerUser for a user', async () => {
+    const user = {
+      firstName: 'Joe',
+      lastName: 'Foo',
+      userName: 'JFoo',
+      email: 'jfoo@gmail.com',
+      password: 'pass123'
+    }
+
+    expect(await controller.registerUser(user.firstName, user.lastName, user.userName, user.email, user.password)).toStrictEqual({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      email: user.email,
+      password: expect.anything(), // we say expect.anything() since the password is going to be hashed.
+      id: expect.any(Number),
+    });
   });
 });
