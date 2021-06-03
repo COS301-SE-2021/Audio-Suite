1ed93e3a785d7ad401d676f0577a5173d153f587
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-06-03 08:41:09 +0200

    Commit for base of tests for office.controller.spec.ts

--- Backend/src/office/office.controller.spec.ts
+++ Backend/src/office/office.controller.spec.ts
@@ -1,13 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OfficeController } from './office.controller';
+import { OfficeService } from './office.service'
 
 describe('OfficeController', () => {
   let controller: OfficeController;
 
+  const mockOfficeService = {
+
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OfficeController],
-    }).compile();
+      providers: [OfficeService],
+    })
+    .overrideProvider(OfficeService)
+    .useValue(mockOfficeService)
+    .compile();
 
     controller = module.get<OfficeController>(OfficeController);
   });
