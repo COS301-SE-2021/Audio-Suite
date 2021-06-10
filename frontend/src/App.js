67e827b79b366d683530958dae427fe4b7aa41e4
Christian Still <u19060123@tuks.co.za>
2021-06-11 00:29:50 +0200

    Added routing for a homePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import SplashPage from './pages/SplashPage'
+import HomePage from './pages/HomePage'
 import { useState } from 'react'
 
 function App() {
-  const [page, setPage] = useState('splashPage')
+  const [page, setPage] = useState('homePage')
 
   const changePage = (pageName) =>
   {
     setPage(pageName)
   }
 
-  if(page === 'splashPage') 
+  if(page === 'homePage')
+  {
+    return (
+      <div>
+        <HomePage changePage={changePage}/>
+      </div>
+    );
+  }
+  else if(page === 'splashPage') 
   {
     return (
       <div>
