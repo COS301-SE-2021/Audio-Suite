531d2389756373e4e24cc9565505384069fb4e72
Christian Still <u19060123@tuks.co.za>
2021-06-11 00:27:32 +0200

    added the navbar component for the homePage and  splashPage

---
+++ frontend/src/components/HomeNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import '../CSS/HomeNavbar.css'
+
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const HomeNavbar = ({onChangePageType, changePage}) => {
+    const navigateToLoginPage = () => 
+    {
+        //onChangePageType('loginPage')
+        changePage('splashPage')
+    }
+
+    return (
+        <>
+            <Navbar bg="light" expand="sm" >
+                <Navbar.Brand><button id="Home-btn" onClick={() => changePage('homePage')}>Audio Suite</button></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Form inline >
+                        <Button variant="outline-success" onClick={() => navigateToLoginPage()}>Log In</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default HomeNavbar
