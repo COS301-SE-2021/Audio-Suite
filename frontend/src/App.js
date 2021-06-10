52abc9eeacee5c425e1d39e33216c9d4c01f72f8
Christian Still <u19060123@tuks.co.za>
2021-06-10 23:26:57 +0200

    imported the bootstrap css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import SplashPage from './pages/SplashPage'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [page, setPage] = useState('splashPage')
+
+  const changePage = (pageName) =>
+  {
+    setPage(pageName)
+  }
+
+  if(page === 'splashPage') 
+  {
+    return (
+      <div>
+        <SplashPage changePage={changePage}/>
+      </div>
+    );
+  }
+  else
+  {
+    return (
+      <div>
+        Not splashPage
+      </div>
+    );
+  }
 }
 
 export default App;
