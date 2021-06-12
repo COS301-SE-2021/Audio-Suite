1e95dae3efb94c1e6ac6f385b949aeb8ea795c41
u19126222 <u19126222@tuks.co.za>
2021-06-12 23:13:14 +0200

    Create UserSettingsPage.js

---
+++ frontend/src/pages/UserSettingsPage.js
@@ -0,0 +1,14 @@
+// import {SettingsPane, SettingsPage, SettingsContent, SettingsMenu} from 'react-settings-pane'
+import React from 'react'
+
+const UserSettingsPage = ({changePage}) => {
+
+    return (
+        <>
+            <h1>Settings</h1>
+        </>
+    )
+
+}
+
+export default UserSettingsPage
