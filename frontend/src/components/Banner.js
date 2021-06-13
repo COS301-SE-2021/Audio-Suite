0848a8e781bb738c00f3eeaf0648af9d02317e33
Christian Still <u19060123@tuks.co.za>
2021-06-13 14:19:40 +0200

    Added a Banner component for the UserPage

---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            <center>
+                <h1 className="Banner-Header">
+                    Audio Suite
+                </h1>    
+            </center> 
+        </div>
+    )
+}
+
+export default Banner
