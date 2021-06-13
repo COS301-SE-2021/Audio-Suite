9fab630ab99c16fdb761f5fabbd26d6fee1db40f
DreamerJ1 <82460229+DreamerJ1@users.noreply.github.com>
2021-06-13 21:30:54 +0200

    Finished coding notifications
    Finished coding notifications, need to start doing testing

--- Backend/src/notification/notification.service.ts
+++ Backend/src/notification/notification.service.ts
@@ -5,6 +5,8 @@ import { create } from 'domain';
 import { UserService } from 'src/user/user.service';
 import { Repository, RepositoryNotFoundError } from 'typeorm';
 import { Notifications } from './notification.entity';
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
 
 @Injectable()
 export class NotificationService {
@@ -44,10 +46,10 @@ export class NotificationService {
 
     //helper function for createNotification
     protected createType(type: string){
-        return null;
+        return type;
     }
     protected getNotificationType(type: string){
-        return null;
+        return type;
     }
     protected addNotification(payload: string, userID: string, createdDateTime: Date, notificationType: string){
         const readDateTime = null;
@@ -61,9 +63,90 @@ export class NotificationService {
         //check for valid user
         try{
             const user = await this.userService.validateUser(jwt);
+
         }catch(err){
             throw new UnauthorizedException;
+
         }
+
+        //send email
+        try{
+            //check that email is valied
+            if(this.validateEmail(emailAddress)){
+
+                //check to make sure the email has body and such
+                if(true){
+
+                    //send email
+                    const hostname = "smtp.office365.com";
+                    const username = "projectpegasusdevs@outlook.com";
+                    const password = "PrOjEcTpEgAsUs";
+            
+                    const transporter = nodemailer.createTransport({
+                        host: hostname,
+                        port: 587,
+                        secure: false,
+                        requireTLS: true,
+                        auth: {
+                            user: username,
+                            pass: password,
+                        },
+                        logger: true
+            
+                    });
+                    const sendEmail = await transporter.sendMail({
+                        from: '"Project Pegasus" <projectpegasusdevs@outlook.com>',
+                        to: emailAddress,
+                        subject: type,
+                        text: payload,
+
+                    });
+
+                    //return the response
+                    return {
+                        status: "Success",
+                        response: "Email Sent",
+                    }
+                    
+
+                } else{
+                    throw ("Missing required information")
+
+                }
+
+            } else{
+                throw ("The email failed to send")
+
+            }
+
+        } catch(err){
+            throw err;
+        }
+    }
+
+    //helper functions for sending emails 
+    protected validateEmail(email: string){
+        const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return true;
+    }
+    protected createTransport(){
+        const hostname = "smtp.office365.com";
+        const username = "projectpegasusdevs@outlook.com";
+        const password = "PrOjEcTpEgAsUs";
+
+        const transporter = nodemailer.createTransport({
+            host: hostname,
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth: {
+                user: username,
+                pass: password,
+            },
+            logger: true
+
+        });
+        return transporter;
     }
 
     //change notification type
