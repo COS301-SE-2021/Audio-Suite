ac231b91c786c6b93deab8ab152a7a3daf22ba90
Christian Still <u19060123@tuks.co.za>
2021-06-13 14:20:06 +0200

    Added the FloorPlan component for the UserPage

---
+++ frontend/src/components/FloorPlan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FloorPlan = () => {
+    return (
+        <div>
+            <h1>This is the Floor Plan tab.</h1>
+        </div>
+    )
+}
+
+export default FloorPlan
