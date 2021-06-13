1e52f65d2540c128c72c6d03198e0c377fc52e0b
DreamerJ1 <82460229+DreamerJ1@users.noreply.github.com>
2021-06-13 15:40:36 +0200

    Notification Controller
    Created the notification controller for the API.

--- Backend/src/notification/notification.controller.ts
+++ Backend/src/notification/notification.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, PayloadTooLargeException, Post } from '@nestjs/common';
+import { NotificationService } from './notification.service';
 
-@Controller('notification')
-export class NotificationController {}
+@Controller('api')
+export class NotificationController {
+    constructor(private readonly notificationService: NotificationService){}
+
+    @Post('notification/createNotification')
+    async createNotification(
+        @Body('userID') userID: string,
+        @Body('type') type: string,
+        @Body('link') link: string,
+        @Body('jwt') jwt: string
+    ){
+        return await this.notificationService.createNotification(userID, type, link, jwt);
+    }
+
+    @Post('notification/sendEmail')
+    async sendEmail(
+        @Body('emailAdress') emailAddress: string,
+        @Body('type') type: string,
+        @Body('payload') payload: string,
+        @Body('jwt') jwt: string
+    ){
+        return await this.notificationService.sendEmail(emailAddress, type, payload, jwt);
+    }
+
+    @Post('notification/changeEmail')
+    async changeEmail(
+        @Body('read') read: boolean,
+        @Body('createdDate') createdDate: Date,
+        @Body('jwt') jwt: string
+    ){
+        return await this.notificationService.changeNotification(read, createdDate, jwt);
+    }
+
+    @Post('notification/retrieveNotification')
+    async retrieveNotification(
+        @Body('userID') userID: string,
+        @Body('unread') unread: boolean,
+        @Body('jwt') jwt: string
+    ){
+        return await this.notificationService.retrieveEmail(userID, unread, jwt);
+    }
+}
--- Backend/src/notification/notification.service.ts
+++ Backend/src/notification/notification.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+    constructor(
+
+    ) {}
+
+    //create the notification
+    async createNotification(userID: string, type: string, link: string, jwt: string) : Promise<any>{
+
+    }
+
+    //sending an email
+    async sendEmail(emailAddress: string, type: string, payload: string, jwt: string) : Promise<any>{
+
+    }
+
+    //change notification type
+    async changeNotification(read: boolean, createdDate: Date, jwt: string) : Promise<any>{
+
+    }
+
+    //receiving notification
+    async retrieveEmail(userID: string, unread: boolean, jwt: string) : Promise<any>{
+
+    }
+}
