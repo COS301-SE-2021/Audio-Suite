19b995b0fd5dbd2c13e15987c9e9d3ba44351fea
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-06-16 18:52:47 +0200

    Modified settings.module.ts to accept TypeORM and UserService implementations.

--- Backend/src/settings/settings.module.ts
+++ Backend/src/settings/settings.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from 'src/user/user.service';
 import { SettingsController } from './settings.controller';
+import { Settings } from './settings.entity';
 import { SettingsService } from './settings.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Settings]),
+  ],
   controllers: [SettingsController],
-  providers: [SettingsService]
+  providers: [SettingsService, UserService]
 })
 export class SettingsModule {}
