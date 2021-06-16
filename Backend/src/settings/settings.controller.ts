a8be2d897cfbef8bd4960748f5db8c9c1fe8115e
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-06-16 19:08:24 +0200

    Creation of changeMode in settings.controller.ts

--- Backend/src/settings/settings.controller.ts
+++ Backend/src/settings/settings.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { SettingsService } from './settings.service';
 
-@Controller('settings')
-export class SettingsController {
-    constructor(private settingsService: SettingsService){}
 
-    @Post('')
-    async getUser(){
-        return this.settingsService.getUser();
+@Controller('api/settings')
+export class SettingsController {
+    constructor(
+        private settingsService: SettingsService,
+        private userService: UserService){}
+
+    // This function is to change your visual appearnce of AudioSuite.
+    @Post('changeMode')
+    async changeMode(
+        @Body('jwt') jwt: string,
+        @Body('mode') mode: boolean
+    ){
+        try {
+            const setting = await this.settingsService.changeMode(jwt, mode);
+            if(setting!=null){
+                return {Response: "Success", userSettings: setting};
+            }else{
+                return {Response: "Unsuccessful"};
+            }
+        } catch (error) {
+            throw new UnauthorizedException();
+        }
     }
 }
