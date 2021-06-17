3a9ad6bd881ece1576be8475b534c55eaf7ffa28
Christian Still <u19060123@tuks.co.za>
2021-06-18 01:50:38 +0200

    added darkmode css and imported it for the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+//import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import './assets/css/mdb.dark.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
