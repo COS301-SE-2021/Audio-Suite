c9a05765ac903cd40365a6b40693b96c1968420e
0xWra1th <danielscragg@outlook.com>
2021-06-18 17:56:42 +0200

    Floorplan Intial Room Implementation

--- frontend/src/components/FloorPlan.js
+++ frontend/src/components/FloorPlan.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const FloorPlan = () => {
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const FloorPlan = ({joinRoom, officeSelected, getRemoteUsers}) => {
     return (
         <div>
-            <h1>This is the Floor Plan tab.</h1>
+            <h1>{officeSelected}</h1>
+            <br></br>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h3>Rooms</h3>
+                        <Button variant="outline-light" block onClick={ async () => {joinRoom("Open Office Space")} }>Open Office Space</Button>
+                        <Button variant="outline-light" block onClick={ async () => {joinRoom("Coffee Station")} }>Coffee Station</Button>
+                        <Button variant="outline-light" block onClick={ async () => {joinRoom("Reception")} }>Reception</Button>
+                        <Button variant="outline-light" block onClick={ async () => {joinRoom("Meeting Room")} }>Meeting Room</Button>
+                    </Col>
+                    <Col>
+                        <h3>Users</h3>
+                        <pre>
+                        {
+                            getRemoteUsers()
+                        }
+                        </pre>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
