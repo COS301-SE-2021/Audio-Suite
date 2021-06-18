8d213fef0b64ac753ec1deb5a4f83315ac5f41f8
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-06-19 00:56:01 +0200

    End-to-end testing for user

--- Backend/test/app.e2e-spec.ts
+++ Backend/test/app.e2e-spec.ts
@@ -1,24 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+const app = 'http://localhost:3000';
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+describe('ROOT', () => {
+  it('should return Hello World', () => {
+    return request(app)
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+});
+
+describe('USER', () => {
+  it('should register a user', () => {
+    const user = {
+      firstName: 'John',
+      lastName: 'Moore',
+      userName: 'JohnMoore',
+      email: 'johnmoore@gmail.com',
+      password: 'Password!123'
+    };
+
+    return request(app)
+      .post('/api/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(HttpStatus.CREATED);
   });
 });
