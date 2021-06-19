45baca7581299b6b33a4fd4e59c19e61549cef7d
0xWra1th <danielscragg@outlook.com>
2021-06-19 19:31:42 +0200

    Enumerate Rooms per Office

--- frontend/src/components/UserSideBar.js
+++ frontend/src/components/UserSideBar.js
@@ -7,8 +7,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-function UserSideBar({officeSelected, setCurrentOffice, leaveOffice, getOffices}) {
-    console.log(officeSelected);
+function UserSideBar({officeSelected, leaveOffice, getOffices, joinOffice}) {
     return(
         <Container fluid>
             <Row>
@@ -20,20 +19,48 @@ function UserSideBar({officeSelected, setCurrentOffice, leaveOffice, getOffices}
                 getOffices()
             }
             <br></br>
+            {
+                officeSelected !== ''
+                &&
+                <>
+                <Row>
+                    <Col>
+                        <h4>Office Settings:</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button variant="success" block onClick={ async () => {} }>Send Invite</Button>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                    <Col>
+                        <Button variant="danger" block onClick={ async () => {await leaveOffice("office")} }>Leave Office</Button>
+                    </Col>
+                </Row>
+                <br></br>
+                <br></br>
+                </>
+            }
             <Row>
                 <Col>
                     <h4>Quick Settings:</h4>
                 </Col>
             </Row>
-            {
-                officeSelected !== ''
-                &&
-                <Row>
-                    <Col>
-                        <Button variant="outline-light" block onClick={ async () => {await leaveOffice("office")} }>Leave Office</Button>
-                    </Col>
-                </Row>
-            }
+            <Row>
+                <Col>
+                    <Button variant="info" block onClick={ async () => {} }>Create Office</Button>
+                </Col>
+            </Row>
+            <br></br>
+            <Row>
+                <Col>
+                    <Button variant="info" block onClick={ async () => {} }>Join Office</Button>
+                </Col>
+            </Row>
+            
+            
         </Container>
     )
 }
