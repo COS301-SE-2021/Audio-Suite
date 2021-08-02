14ab2537b307f61f7be0f42b61ecee44bd938e48
Christian Still <u19060123@tuks.co.za>
2021-08-02 22:01:16 +0200

    Added the sidebar and the tabs for user page for text, floorplan, kanban

--- frontend-Angular/src/app/pages/user/user.component.ts
+++ frontend-Angular/src/app/pages/user/user.component.ts
@@ -5,11 +5,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
+
 export class UserComponent implements OnInit {
+
+  sidebarOpend = true;
+  showOfficeList = true;
+  officeSelected = false;
+  selectedOffice = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.add("user-page");
   }
 
+  toggleSidebar(): void {
+    this.sidebarOpend = !this.sidebarOpend;
+  }
+
+  selectOffice(office): void{
+    this.selectedOffice = office;
+    this.officeSelected = true;
+  }
+
+  toggleOfficeListView(): void{
+    if(this.showOfficeList){
+      document.getElementById('officeList').classList.replace("showOffices", "hideOffices");
+      document.getElementById('officeListIcon').classList.replace("icon-minimal-down", "icon-minimal-right");
+      this.showOfficeList = false;
+    }
+    else{
+      document.getElementById('officeList').classList.replace("hideOffices", "showOffices");
+      document.getElementById('officeListIcon').classList.replace("icon-minimal-right", "icon-minimal-down");
+      this.showOfficeList = true;
+    }
+  }
+
+  ngOnDestroy() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("user-page");
+  }
 }
