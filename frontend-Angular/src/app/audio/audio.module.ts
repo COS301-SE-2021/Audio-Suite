b9924d04be63f7aae5c147aae8f9cd801655f652
u19126222 <u19126222@tuks.co.za>
2021-08-04 12:15:46 +0200

    Update audio.module.ts declarations

--- frontend-Angular/src/app/audio/audio.module.ts
+++ frontend-Angular/src/app/audio/audio.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxAgoraModule } from 'ngx-agora';
 import { AppRoutingModule } from '../app-routing.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
@@ -12,7 +13,8 @@ import { AppRoutingModule } from '../app-routing.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    NgxAgoraModule.forRoot({ AppID: '023766436b244044ab85f65470dcbae2' })
+    //NgxAgoraModule.forRoot({ AppID: '023766436b244044ab85f65470dcbae2' })
+    NgxAgoraModule.forRoot({ AppID: environment.agora.appId })
   ]
 })
 export class AudioModule { }
