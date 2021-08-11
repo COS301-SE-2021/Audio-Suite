433bad48e24d3370167ffbc2a2c9bde1b17497be
u19126222 <u19126222@tuks.co.za>
2021-08-11 22:16:38 +0200

    Update app.module.ts

--- frontend-Angular/src/app/app.module.ts
+++ frontend-Angular/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { ProgressbarModule } from "ngx-bootstrap/progressbar";
@@ -24,12 +26,20 @@ import { IndexComponent } from "./pages/index/index.component";
 import { ProfilepageComponent } from "./pages/examples/profilepage/profilepage.component";
 import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
 import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
+import { AudioComponent } from './audio/audio.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:3001/serversocket', options: {} };
+
+const agoraConfig: AgoraConfig = {
+  AppID: '7afb53157f754f6f8023f31fb343404a',
+};
+//023766436b244044ab85f65470dcbae2
+//7afb53157f754f6f8023f31fb343404a
 
 @NgModule({
   declarations: [
     AppComponent,
+    AudioComponent,
     //FormModalComponent
     // IndexComponent,
     // ProfilepageComponent,
@@ -54,6 +64,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3001/serversocket', opti
     // BsDatepickerModule.forRoot(),
     // CarouselModule.forRoot(),
     // ModalModule.forRoot()
+    BrowserModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
