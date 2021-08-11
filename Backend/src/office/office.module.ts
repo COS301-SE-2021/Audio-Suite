8982607c12c41726823350a771c0b24d6164b1b7
Christian Still <u19060123@tuks.co.za>
2021-08-11 22:12:25 +0200

    Added room, roomUsers to imports and their services to provider
    To complete the circular dependency loop

--- Backend/src/office/office.module.ts
+++ Backend/src/office/office.module.ts
@@ -8,17 +8,23 @@ import {JwtModule} from "@nestjs/jwt";
 import { User } from '../user/user.entity';
 import { OfficeUsers } from '../officeusers/officeusers.entity';
 import { OfficeusersService } from '../officeusers/officeusers.service';
+import { RoomService } from 'src/room/room.service';
+import { Room } from 'src/room/room.entity';
+import { RoomUsers } from 'src/roomusers/roomusers.entity';
+import { RoomUsersService } from 'src/roomusers/roomusers.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Office]),
   TypeOrmModule.forFeature([User]),
   TypeOrmModule.forFeature([OfficeUsers]),
+  TypeOrmModule.forFeature([Room]),
+  TypeOrmModule.forFeature([RoomUsers]),
   JwtModule.register({
     secret: 'secret', // TODO change to env var
     signOptions: {expiresIn: '1d'}
   })
 ],
   controllers: [OfficeController],
-  providers: [Office, OfficeService, UserService, OfficeusersService]
+  providers: [Office, OfficeService, UserService, OfficeusersService, RoomService, RoomUsersService],
 })
 export class OfficeModule {}
