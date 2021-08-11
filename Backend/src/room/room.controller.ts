ca4a5526ab5523f7d191eac90f6acb22253fab12
Christian Still <u19060123@tuks.co.za>
2021-08-11 22:14:15 +0200

    Added new endpoint updateRoomDetails to update the coordinates of a room

--- Backend/src/room/room.controller.ts
+++ Backend/src/room/room.controller.ts
@@ -20,9 +20,28 @@ export class RoomController {
         @Body('jwt') jwt: string,
         @Body('officeID') officeID: number,
         @Body('roomName') roomName: string, 
+        @Body('xCoordinate') xCoordinate: number,
+        @Body('yCoordinate') yCoordinate: number,
+        @Body('width') width: number,
+        @Body('height') height: number,
     )
     {
-        return await this.roomService.registerRoom(jwt, officeID, roomName);
+        return await this.roomService.registerRoom(jwt, officeID, roomName, xCoordinate, yCoordinate, width, height);
+    }
+
+    @Post('room/updateRoomDetails')
+    async updateRoomDetails(
+        @Body('jwt') jwt: string,
+        @Body('roomID') roomID: number,
+        @Body('officeID') officeID: number,
+        @Body('roomName') roomName: string, 
+        @Body('xCoordinate') xCoordinate: number,
+        @Body('yCoordinate') yCoordinate: number,
+        @Body('width') width: number,
+        @Body('height') height: number,
+    )
+    {
+        
     }
 
     //join room
