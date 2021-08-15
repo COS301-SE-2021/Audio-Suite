9dccbea5af61720bd0b055eda4376ddf3eb4600c
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-08-15 11:35:44 +0100

    Creation of Kanban Services in frontend to handle routing and functionality

---
+++ frontend-Angular/src/app/services/kanban.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs'
+
+const httpOptions = {
+  headers: new HttpHeaders({
+     'Content-Type': 'application/json'
+    }),
+}
+
+const baseUrl = 'http://localhost:3001'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KanbanService {
+
+  constructor(private http: HttpClient) { }
+
+  createCard(
+    jwt: string, 
+    officeID: number, 
+    listName:string, 
+    cardID: string, 
+    cardMessage: string
+  ): Observable<any>{
+      const body = {
+          jwt: jwt,
+          officeID: officeID,
+          listName: listName,
+          cardID: cardID,
+          cardMessage: cardMessage
+      }
+      var createCardURL = baseUrl + '/api/user';
+      return this.http.post<any>(createCardURL, body, httpOptions);
+  }
+
+}
