0826e38d8602179dd8383b05b2be1bb710089561
Christian Still <u19060123@tuks.co.za>
2021-08-17 23:15:34 +0200

    Setup the testing module and a test to check if the component is working

--- frontend-Angular/src/app/services/office-room.service.spec.ts
+++ frontend-Angular/src/app/services/office-room.service.spec.ts
@@ -1,16 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 
 import { OfficeRoomService } from './office-room.service';
 
 describe('OfficeRoomService', () => {
-  let service: OfficeRoomService;
+  let httpClient: HttpClient;
+  let httpTestCtrl: HttpTestingController;
+  let officeRoomService: OfficeRoomService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OfficeRoomService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestCtrl = TestBed.get(HttpTestingController);
   });
 
+  beforeEach(inject(
+    [OfficeRoomService], (service: OfficeRoomService) => {
+      officeRoomService = service;
+    }
+  ));
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(officeRoomService).toBeTruthy();
   });
 });
