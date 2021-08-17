a92b5c933a43ccea5ad893ee3bb152e887fa085b
Christian Still <u19060123@tuks.co.za>
2021-08-17 23:25:45 +0200

    fixed the office service to allow for simpler testing

--- frontend-Angular/src/app/services/office-room.service.spec.ts
+++ frontend-Angular/src/app/services/office-room.service.spec.ts
@@ -26,4 +26,31 @@ describe('OfficeRoomService', () => {
   it('should be created', () => {
     expect(officeRoomService).toBeTruthy();
   });
+
+  it('should register a new office', () => {
+    const expectedResponse = {
+      Response: "Success",
+      Office: {
+        id: "1", 
+        name: "test_office",
+        invite: "hsjklgahs6w37e8h3jk6ui23jkh6"
+      }
+    };
+    
+    const jwt = 'gsahjgkhsdaklfhalskfklas.ghaslkghslslghsal.gfhaslkg/agfhilashglkas';
+    const officeName = 'test_office';
+    officeRoomService.registerOffice(jwt, officeName).subscribe((response) => {
+      expect(expectedResponse).toBe(response, "should compare expected response to actual response");
+    }, 
+    (error) => {
+      console.log(error);
+    });
+
+    const req = httpTestCtrl.expectOne(officeRoomService.baseUrl + '/api/office/register');
+
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+
+    req.flush(expectedResponse);
+  });
 });
