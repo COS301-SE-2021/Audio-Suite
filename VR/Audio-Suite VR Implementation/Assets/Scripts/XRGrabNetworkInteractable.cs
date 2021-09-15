f3151ce4eacfcf947a9d3c64fc1e3ae8abd99846
DreamerJ1 <82460229+DreamerJ1@users.noreply.github.com>
2021-09-15 14:46:56 +0200

    New Test Build

--- VR/Audio-Suite VR Implementation/Assets/Scripts/XRGrabNetworkInteractable.cs
+++ VR/Audio-Suite VR Implementation/Assets/Scripts/XRGrabNetworkInteractable.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class XRGrabNetworkInteractable : MonoBehaviour
+using Photon.Pun;
+
+public class XRGrabNetworkInteractable : XRGrabInteractable
 {
+    private PhotonView photonView;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        photonView = GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
     {
-        
+        photonView.RequestOwnership();
+        base.OnSelectEntered(interactor);
+    }
+
+    protected override void OnSelectEntering(XRBaseInteractor interactor)
+    {
+        photonView.RequestOwnership();
+        base.OnSelectEntering(interactor);
     }
 }
