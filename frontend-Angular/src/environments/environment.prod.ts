9da0e696fa2867f0944497e7b29448a917945125
Tetalesti <danielscragg@outlook.com>
2021-09-18 21:22:09 +0200

    New landing page

--- frontend-Angular/src/environments/environment.prod.ts
+++ frontend-Angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001',
+  apiUrl: 'https://audiosuite.herokuapp.com',
   agoraAppId: '7afb53157f754f6f8023f31fb343404a',
   tokenUrl : 'https://audiosuite.xyz/fetch_rtc_token'
 };
