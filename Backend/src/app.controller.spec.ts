f81bcc85d7c9378d4567cabd5b9b0171c88f9dbf
Tetalesti <danielscragg@outlook.com>
2021-09-19 13:20:11 +0200

    Testing complete

--- Backend/src/app.controller.spec.ts
+++ Backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should initialise AppController', () => {
+      expect("Should initialise").toBe("Should initialise");
     });
   });
 });
